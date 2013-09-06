056758f1a5eee415d4bd4907f3a1f8eca1d74417
naoki kodama <naoki.koda@gmail.com>
2013-09-07 00:24:43 +0900

    add precompile false.

--- config/application.rb
+++ config/application.rb
@@ -46,5 +46,9 @@ module Todolist
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # for Heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
